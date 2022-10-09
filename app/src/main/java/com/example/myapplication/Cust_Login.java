6a6711be2aeb31398ebc58b14fcb3aeec5af72ef
Akshay Kudtarkar <akshaykudtarkar9892@gmail.com>
2022-10-09 23:35:00 +0530

    Login & Register Done !!

--- app/src/main/java/com/example/myapplication/Cust_Login.java
+++ app/src/main/java/com/example/myapplication/Cust_Login.java
@@ -1,20 +1,49 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Cust_Login extends AppCompatActivity {
+
+    private EditText editTextLoginEmail, editTextLoginPwd;
+    private TextView buttonforgetpassword,buttonlogin;
+    private ProgressBar progressBar;
+    private FirebaseAuth authProfile;
+    private static final String TAG = "LoginActivity";
 
     ImageView imageView;
     LinearLayout layout2;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,5 +72,130 @@ public class Cust_Login extends AppCompatActivity {
             }
         });
 
+        editTextLoginEmail = findViewById(R.id.editText_login_email);
+        editTextLoginPwd = findViewById(R.id.editText_login_pwd);
+        progressBar = findViewById(R.id.progressBar);
+
+        authProfile = FirebaseAuth.getInstance();
+
+        TextView buttonForgotPassword = findViewById(R.id.button_forget_password);
+        buttonForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Cust_Login.this, "You can reset your password now!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Cust_Login.this, ForgotPasswordActivity.class));
+            }
+        });
+        ImageView imageViewShowHidePwd = findViewById(R.id.imageView_show_hide_pwd);
+        imageViewShowHidePwd.setImageResource(R.drawable.ic_hide_pwd);
+        imageViewShowHidePwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(editTextLoginPwd.getTransformationMethod().equals(HideReturnsTransformationMethod.getInstance())){
+                    editTextLoginPwd.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    imageViewShowHidePwd.setImageResource(R.drawable.ic_hide_pwd);
+                } else{
+                    editTextLoginPwd.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    imageViewShowHidePwd.setImageResource(R.drawable.ic_show_pwd);
+                }
+            }
+        });
+
+        TextView buttonLogin = findViewById(R.id.button_login);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String textEmail = editTextLoginEmail.getText().toString();
+                String textPwd = editTextLoginPwd.getText().toString();
+
+                if(TextUtils.isEmpty(textEmail)){
+                    Toast.makeText(Cust_Login.this, "Please enter your email", Toast.LENGTH_SHORT).show();
+                    editTextLoginEmail.setError("Email is required");
+                    editTextLoginEmail.requestFocus();
+                } else if(!Patterns.EMAIL_ADDRESS.matcher(textEmail).matches()){
+                    Toast.makeText(Cust_Login.this, "Please re-enter your email", Toast.LENGTH_SHORT).show();
+                    editTextLoginEmail.setError("Valid Email is required");
+                    editTextLoginEmail.requestFocus();
+                } else if(TextUtils.isEmpty(textPwd)){
+                    Toast.makeText(Cust_Login.this, "Please enter your password", Toast.LENGTH_SHORT).show();
+                    editTextLoginPwd.setError("Password is required");
+                    editTextLoginPwd.requestFocus();
+                } else{
+                    progressBar.setVisibility(View.VISIBLE);
+                    loginUser(textEmail, textPwd);
+                }
+            }
+        });
+
+    }
+
+    private void loginUser(String email, String pwd) {
+        authProfile.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(Cust_Login.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+
+
+                    FirebaseUser firebaseUser = authProfile.getCurrentUser();
+
+                    if(firebaseUser.isEmailVerified()){
+                        Toast.makeText(Cust_Login.this, "You are Logged in now", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(Cust_Login.this, Cust_DashBoard.class));
+                        finish();
+                    } else{
+                        firebaseUser.sendEmailVerification();
+                        authProfile.signOut();
+                        showAlertDialog();
+                    }
+                } else{
+                    try {
+                        throw task.getException();
+                    }catch (FirebaseAuthInvalidUserException e){
+                        editTextLoginEmail.setError("User does not exists or is no longer valid. Please register again.");
+                        editTextLoginEmail.requestFocus();
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        editTextLoginEmail.setError("Invalid credentials. Kindly, check and re-enter");
+                        editTextLoginEmail.requestFocus();
+                    } catch (Exception e){
+                        Log.e(TAG, e.getMessage());
+                        Toast.makeText(Cust_Login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                    Toast.makeText(Cust_Login.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                }
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Cust_Login.this);
+        builder.setTitle("Email Not Verified");
+        builder.setMessage("Please veriy your email now. You can not login without email verification");
+
+        builder.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_APP_EMAIL);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        if(authProfile.getCurrentUser()!=null){
+            Toast.makeText(Cust_Login.this, "Already Logged IN!", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(Cust_Login.this, Cust_DashBoard.class));
+            finish();
+        }else{
+            Toast.makeText(Cust_Login.this, "You can login now", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/Cust_Signup.java
+++ app/src/main/java/com/example/myapplication/Cust_Signup.java
@@ -1,18 +1,57 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Cust_Signup extends AppCompatActivity {
 
+    private EditText editTextRegisterFullName, editTextRegisterEmail, editTextRegisterDoB, editTextRegisterMobile, editTextRegisterPwd;
+    private TextView buttonregister;
+    private ProgressBar progressBar;
+    private RadioGroup radioGroupRegisterGender;
+    private RadioButton radioButtonRegisterGenderSelected;
+    private DatePickerDialog picker;
+    private static final String TAG = "Cust_Signup";
+
     ImageView imageView;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,5 +71,179 @@ public class Cust_Signup extends AppCompatActivity {
             }
         });
 
+        getSupportActionBar().setTitle("Register");
+
+        Toast.makeText(Cust_Signup.this, "You can register now", Toast.LENGTH_SHORT).show();
+
+        progressBar = findViewById(R.id.progressBar);
+        editTextRegisterFullName = findViewById(R.id.editText_register_full_name);
+        editTextRegisterEmail = findViewById(R.id.editText_register_email);
+        editTextRegisterDoB = findViewById(R.id.editText_register_dob);
+        editTextRegisterMobile = findViewById(R.id.editText_register_mobile);
+        editTextRegisterPwd = findViewById(R.id.editText_register_password);
+//       editTextRegisterConfirmPwd = findViewById(R.id.editText_register_confirm_password);
+
+        radioGroupRegisterGender = findViewById(R.id.radio_group_register_gender);
+        radioGroupRegisterGender.clearCheck();
+
+        editTextRegisterDoB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+
+                picker = new DatePickerDialog(Cust_Signup.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        editTextRegisterDoB.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
+                    }
+                }, year, month, day);
+                picker.show();
+            }
+        });
+
+        TextView buttonRegister = findViewById(R.id.button_register);
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedGenderId = radioGroupRegisterGender.getCheckedRadioButtonId();
+                radioButtonRegisterGenderSelected = findViewById(selectedGenderId);
+
+                String textFullName = editTextRegisterFullName.getText().toString();
+                String textEmail = editTextRegisterEmail.getText().toString();
+                String textDoB = editTextRegisterDoB.getText().toString();
+                String textMobile = editTextRegisterMobile.getText().toString();
+                String textPwd = editTextRegisterPwd.getText().toString();
+//                String textConfirmPwd = editTextRegisterConfirmPwd.getText().toString();
+                String textGender;
+
+                String mobileRegex = "[6-9][0-9]{9}";
+                Matcher mobileMatcher;
+                Pattern mobilePattern = Pattern.compile(mobileRegex);
+                mobileMatcher = mobilePattern.matcher(textMobile);
+
+                if(TextUtils.isEmpty(textFullName)){
+                    Toast.makeText(Cust_Signup.this, "Please enter your full name", Toast.LENGTH_SHORT).show();
+                    editTextRegisterFullName.setError("Full Name is required");
+                    editTextRegisterFullName.requestFocus();
+                } else if(TextUtils.isEmpty(textEmail)){
+                    Toast.makeText(Cust_Signup.this, "Please enter your email", Toast.LENGTH_SHORT).show();
+                    editTextRegisterEmail.setError("Email is required");
+                    editTextRegisterEmail.requestFocus();
+                } else if(!Patterns.EMAIL_ADDRESS.matcher(textEmail).matches()){
+                    Toast.makeText(Cust_Signup.this, "Please re-enter your email", Toast.LENGTH_SHORT).show();
+                    editTextRegisterEmail.setError("Valid Email is required");
+                    editTextRegisterEmail.requestFocus();
+                } else if(TextUtils.isEmpty(textDoB)) {
+                    Toast.makeText(Cust_Signup.this, "Please enter your date of birth", Toast.LENGTH_SHORT).show();
+                    editTextRegisterDoB.setError("Date of Birth is required");
+                    editTextRegisterDoB.requestFocus();
+                } else if(radioGroupRegisterGender.getCheckedRadioButtonId() == -1) {
+                    Toast.makeText(Cust_Signup.this, "Please select your gender", Toast.LENGTH_SHORT).show();
+                    radioButtonRegisterGenderSelected.setError("Gender is required");
+                    radioButtonRegisterGenderSelected.requestFocus();
+                } else if(TextUtils.isEmpty(textMobile)){
+                    Toast.makeText(Cust_Signup.this, "Please enter your mobile no.", Toast.LENGTH_SHORT).show();
+                    editTextRegisterMobile.setError("Mobile No. is required");
+                    editTextRegisterMobile.requestFocus();
+                } else if(textMobile.length()!=10){
+                    Toast.makeText(Cust_Signup.this, "Please re-enter your mobile no.", Toast.LENGTH_SHORT).show();
+                    editTextRegisterMobile.setError("Mobile No. should be 10 digits");
+                    editTextRegisterMobile.requestFocus();
+                } else if(!mobileMatcher.find()){
+                    Toast.makeText(Cust_Signup.this, "Please re-enter your mobile no.", Toast.LENGTH_SHORT).show();
+                    editTextRegisterMobile.setError("Mobile No. is not valid");
+                    editTextRegisterMobile.requestFocus();
+                } else if(TextUtils.isEmpty(textPwd)){
+                    Toast.makeText(Cust_Signup.this, "Please enter your password", Toast.LENGTH_SHORT).show();
+                    editTextRegisterPwd.setError("Password is required");
+                    editTextRegisterPwd.requestFocus();
+                } else if(textPwd.length()<6){
+                    Toast.makeText(Cust_Signup.this, "Please should be at least 6 digits", Toast.LENGTH_SHORT).show();
+                    editTextRegisterPwd.setError("Password too weak");
+                    editTextRegisterPwd.requestFocus();
+//                } else if(TextUtils.isEmpty(textConfirmPwd)){
+//                    Toast.makeText(Cust_Signup.this, "Please confirm your password", Toast.LENGTH_SHORT).show();
+//                    editTextRegisterConfirmPassword.setError("Password Confirmation is required");
+//                    editTextRegisterConfirmPassword.requestFocus();
+//                } else if(!textPwd.equals(textConfirmPwd)){
+//                    Toast.makeText(Cust_Signup.this, "Please enter same password", Toast.LENGTH_SHORT).show();
+//                    editTextRegisterConfirmPassword.setError("Password Confirmation is required");
+//                    editTextRegisterConfirmPassword.requestFocus();
+//
+//                    editTextRegisterPwd.clearComposingText();
+//                    editTextRegisterConfirmPassword.clearComposingText();
+                }
+                else {
+                    textGender = radioButtonRegisterGenderSelected.getText().toString();
+                    progressBar.setVisibility(View.VISIBLE);
+
+                    registerUser(textFullName, textEmail, textDoB, textGender, textMobile, textPwd);
+                }
+            }
+        });
+
     }
+
+
+    private void registerUser(String textFullName, String textEmail, String textDoB, String textGender, String textMobile, String textPwd) {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.createUserWithEmailAndPassword(textEmail, textPwd).addOnCompleteListener(Cust_Signup.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+
+                    FirebaseUser firebaseUser = auth.getCurrentUser();
+
+                    UserProfileChangeRequest profileChangeRequest = new UserProfileChangeRequest.Builder().setDisplayName(textFullName).build();
+                    firebaseUser.updateProfile(profileChangeRequest);
+
+                    ReadWriteUserDetails writeUserDetails = new ReadWriteUserDetails(textDoB, textGender, textMobile);
+                    DatabaseReference referenceProfile = FirebaseDatabase.getInstance().getReference("Registered Users");
+
+                    referenceProfile.child(firebaseUser.getUid()).setValue(writeUserDetails).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if(task.isSuccessful()){
+                                firebaseUser.sendEmailVerification();
+                                Toast.makeText(Cust_Signup.this, "User registered successfully. please verify your email", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(Cust_Signup.this, Shop_Dashboard.class);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                                finish();
+
+                            } else{
+                                Toast.makeText(Cust_Signup.this, "User registered failed. please try again", Toast.LENGTH_SHORT).show();
+
+                            }
+                            progressBar.setVisibility(View.GONE);
+
+                        }
+                    });
+                } else {
+                    try {
+                        throw task.getException();
+                    } catch (FirebaseAuthWeakPasswordException e){
+                        editTextRegisterPwd.setError("Your password is too weak. Kindly use a mix of alphabets, numbers and special characters");
+                        editTextRegisterPwd.requestFocus();
+                    } catch (FirebaseAuthInvalidCredentialsException e){
+                        editTextRegisterPwd.setError("Your email is invalid or already in use. Kindly re-enter.");
+                        editTextRegisterPwd.requestFocus();
+                    } catch (FirebaseAuthUserCollisionException e){
+                        editTextRegisterPwd.setError("User is already registered with this email. Use another email.");
+                        editTextRegisterPwd.requestFocus();
+                    } catch (Exception e){
+                        Log.e(TAG, e.getMessage());
+                        Toast.makeText(Cust_Signup.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+
 }
